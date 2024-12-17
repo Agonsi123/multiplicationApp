5d05367e0288d9d319b9153f0e625458a81ad879
Agonsi123 <agonsiudodirim@gmail.com>
2024-12-17 17:06:08 +0100

    edit on js file

--- mApp.js
+++ mApp.js
@@ -6,27 +6,38 @@ const formE1 = document.getElementById("form");
 const scoreE1 = document.getElementById("score");
 const questionE1 = document.getElementById("question");
 const inputE1 = document.getElementById("input");
+
 let score = JSON.parse(localStorage.getItem("score"));
 
-if (!score) {
+// if (!score) {
+//     score = 0;
+// }
+//Reset score when page is reloaded
+document.addEventListener("DOMContentLoaded", () => {
     score = 0;
-}
+    updateLocalStorage();
+    scoreE1.innerText = `score: ${score}`;
+});
 
-scoreE1.innerText = `score: ${score}`;
+// scoreE1.innerText = `score: ${score}`;
 
 questionE1.innerText = `What is ${numb1} multiply by ${numb2}?`;
 
 const correctAns = numb1 * numb2;
 
-formE1.addEventListener("submit", () => {
-    const userAns = +inputE1.value;
+formE1.addEventListener("submit", (e) => {
+    e.preventDefault(); //prevent form submission refresh
+    const userAns = +inputE1.value; //Convert input to a number
     if (userAns === correctAns) {
         score++;
-        updateLocalStorage();
+        // updateLocalStorage();
     } else {
         score--;
-        updateLocalStorage();
+        // updateLocalStorage();
     }
+    updateLocalStorage();
+    scoreE1.innerText = `score: ${score}`;
+    inputE1.value = ""; //clear input field
 });
 
 function updateLocalStorage() {
