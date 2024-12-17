01a3c61bb8bf078ea2d9ddda1265d5f7b995ad36
Agonsi123 <agonsiudodirim@gmail.com>
2024-12-17 17:14:19 +0100

    edit 2

--- mApp.js
+++ mApp.js
@@ -9,9 +9,9 @@ const inputE1 = document.getElementById("input");
 
 let score = JSON.parse(localStorage.getItem("score"));
 
-// if (!score) {
-//     score = 0;
-// }
+if (!score) {
+    score = 0;
+}
 //Reset score when page is reloaded
 document.addEventListener("DOMContentLoaded", () => {
     score = 0;
@@ -19,25 +19,21 @@ document.addEventListener("DOMContentLoaded", () => {
     scoreE1.innerText = `score: ${score}`;
 });
 
-// scoreE1.innerText = `score: ${score}`;
+scoreE1.innerText = `score: ${score}`;
 
 questionE1.innerText = `What is ${numb1} multiply by ${numb2}?`;
 
 const correctAns = numb1 * numb2;
 
-formE1.addEventListener("submit", (e) => {
-    e.preventDefault(); //prevent form submission refresh
+formE1.addEventListener("submit", () => {
     const userAns = +inputE1.value; //Convert input to a number
     if (userAns === correctAns) {
         score++;
-        // updateLocalStorage();
+        updateLocalStorage();
     } else {
         score--;
-        // updateLocalStorage();
+        updateLocalStorage();
     }
-    updateLocalStorage();
-    scoreE1.innerText = `score: ${score}`;
-    inputE1.value = ""; //clear input field
 });
 
 function updateLocalStorage() {
